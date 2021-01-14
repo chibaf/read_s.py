1e7f709f997b372cecc882772aa7ab87c1912d97
chibaf <chibaf@mac.com>
2021-01-15 00:10:49 +0900

    Add files via upload

--- read_s2.py
+++ read_s2.py
@@ -2,14 +2,6 @@ import serial, sys
 
 strPort = sys.argv[1]
 
-#ser = serial.Serial(
-#    port=strPort,\
-#    baudrate=sys.argv[2],\
-#    parity=serial.PARITY_NONE,\
-#    stopbits=serial.STOPBITS_ONE,\
-#    bytesize=serial.EIGHTBITS,\
-#        timeout=0)
-
 ser=serial.Serial(strPort, sys.argv[2])
 
 print("connected to: " + ser.portstr)
