aa977bd6f40410d2734637ad57fe712fb09ff7c3
chibaf <chibaf@mac.com>
2021-01-15 00:37:50 +0900

    Add files via upload

--- read_s.py
+++ read_s.py
@@ -5,11 +5,9 @@ strPort = sys.argv[1]
 ser=serial.Serial(strPort, sys.argv[2])
 
 print("connected to: " + ser.portstr)
-count=1
 
 while True:
   line = ser.readline()
   print(line)
-  count = count+1
 
 ser.close()
